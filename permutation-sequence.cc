b2f9329101d0b6647fdeab5847909ae6381d0587
Ray Song <i@maskray.me>
2014-08-20 08:03:12 +0800

    permutation-sequence: 陈霜

--- permutation-sequence.cc
+++ permutation-sequence.cc
@@ -44,3 +44,24 @@ public:
     return r;
   }
 };
+
+// 陈霜
+
+#define ROF(i, a, b) for (int i = (b); --i >= (a); )
+
+class Solution {
+public:
+  string getPermutation(int n, int k) {
+    int f[] = {1,1,2,6,24,120,720,5040,40320};
+    long long a = 0x987654321;
+    string r;
+    k--;
+    ROF(i, 0, n) {
+      int t = k/f[i];
+      k %= f[i];
+      r += '0'|a>>4*t&15;
+      a = a&(1LL<<4*t)-1|a>>4*(t+1)<<4*t;
+    }
+    return r;
+  }
+};

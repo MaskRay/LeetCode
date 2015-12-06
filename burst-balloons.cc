78613d8e9b00e4ffa842d977947d1f2f08d461a6
Ray Song <i@maskray.me>
2015-12-06 22:06:00 +0800

    .

--- burst-balloons.cc
+++ burst-balloons.cc
@@ -1,4 +1,6 @@
 // Burst Balloons
+// dynamic programming
+// NB. F. Frances Yao's optimization does not apply
 #define FOR(i, a, b) for (int i = (a); i < (b); i++)
 #define REP(i, n) for (int i = 0; i < (n); i++)
 #define ROF(i, a, b) for (int i = (b); --i >= (a); )
@@ -10,18 +12,31 @@ public:
     if (! n)
       return 0;
     vector<vector<int>> dp(n, vector<int>(n, 0));
-    REP(i, n)
-      dp[i][i] = (i ? nums[i-1] : 1) * nums[i] * (i+1 < n ? nums[i+1] : 1);
-    ROF(i, 0, n-1)
-      FOR(j, i+1, n) {
-        int opt = -1, optk;
-        FOR(k, i, j+1) {
-          int t = (k ? dp[i][k-1] : 0) + (k+1 < n ? dp[k+1][j] : 0) + (i ? nums[i-1] : 1) * nums[k] * (j+1 < n ? nums[j+1] : 1);
-          if (t > opt)
-            opt = t;
-        }
-        dp[i][j] = opt;
+    ROF(i, 0, n)
+      FOR(j, i, n) {
+        FOR(k, i, j+1)
+          dp[i][j] = max(dp[i][j], (k ? dp[i][k-1] : 0) + (k+1 < n ? dp[k+1][j] : 0) + (i ? nums[i-1] : 1) * nums[k] * (j+1 < n ? nums[j+1] : 1));
       }
     return dp[0][n-1];
+  }
+};
+
+/// with padding
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define ROF(i, a, b) for (int i = (b); --i >= (a); )
+
+class Solution {
+public:
+  int maxCoins(vector<int>& nums) {
+    int n = nums.size();
+    nums.insert(nums.begin(), 1);
+    nums.push_back(1);
+    vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+    ROF(i, 1, n+1)
+      FOR(j, i, n+1)
+        FOR(k, i, j+1)
+          dp[i][j] = max(dp[i][j], dp[i][k-1]+dp[k+1][j]+nums[i-1]*nums[k]*nums[j+1]);
+    return dp[1][n];
   }
 };

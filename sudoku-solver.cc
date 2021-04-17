b038d428098e682311d11b8c9e1de54f95559b6d
Fangrui Song <i@maskray.me>
2021-04-17 13:36:10 -0700

    sudoku-solver

--- sudoku-solver.cc
+++ sudoku-solver.cc
@@ -1,11 +1,11 @@
 // Sudoku Solver
+/// exact cover problem
 #define REP(i, n) for (int i = 0; i < (n); i++)
 
 class Solution {
 private:
-  static const int N = 9, MAXR = N*N*N, MAXC = N*N*4, M = MAXC+1+MAXR*4;
-  int L[M], R[M], U[M], D[M], col[M], size[M], res[N*N];
-  pair<int, int> mean[M];
+  static const int N = 9, MAXC = N*N*4, M = MAXC+1+N*N*N*4;
+  int L[M], R[M], U[M], D[M], col[M], size[M], mean[M];
   void cover(int c) {
     L[R[c]] = L[c];
     R[L[c]] = R[c];
@@ -21,7 +21,7 @@ private:
         U[D[j]] = D[U[j]] = j;
     L[R[c]] = R[L[c]] = c;
   }
-  void insert(int i, int l, int r, int c) {
+  void append(int i, int l, int r, int c) {
     L[i] = l; R[i] = r;
     D[U[i] = U[c]] = i;
     U[D[i] = c] = i;
@@ -39,7 +39,7 @@ private:
     for (int i = D[c]; i != c; i = D[i]) {
       for (int j = R[i]; j != i; j = R[j])
         cover(col[j]);
-      a[mean[i].first/9][mean[i].first%9] = '1'+mean[i].second;
+      a[mean[i]/N/N][mean[i]/N%N] = '1'+mean[i]%N;
       if (dlx(a)) return true;
       for (int j = L[i]; j != i; j = L[j])
         uncover(col[j]);
@@ -67,11 +67,11 @@ public:
           hi = 9;
         }
         for (int k = lo; k < hi; k++) {
-          pair<int, int> me = make_pair(i*N+j, k);
-          insert(cur, cur+3, cur+1, i*N+k); mean[cur] = me;
-          insert(cur+1, cur, cur+2, N*N+j*N+k); mean[cur+1] = me;
-          insert(cur+2, cur+1, cur+3, N*N*2+i*N+j); mean[cur+2] = me;
-          insert(cur+3, cur+2, cur, N*N*3+(i/3*3+j/3)*N+k); mean[cur+3] = me;
+          append(cur, cur+3, cur+1, i*N+k);
+          append(cur+1, cur, cur+2, N*N+j*N+k);
+          append(cur+2, cur+1, cur+3, N*N*2+i*N+j);
+          append(cur+3, cur+2, cur, N*N*3+(i/3*3+j/3)*N+k);
+          fill_n(mean+cur, 4, (i*N+j)*N+k);
           cur += 4;
         }
       }
